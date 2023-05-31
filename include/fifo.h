6a7fd0b8b0450a4b1b4187a532203dd5443c6427
Joao-Lamounier <jjoaolamounier@gmail.com>
2023-05-30 22:56:55 -0300

    Implementado fila de navio

---
+++ include/fifo.h
@@ -0,0 +1,23 @@
+#ifndef PORTSIMULATOR_FIFO_H
+#define PORTSIMULATOR_FIFO_H
+
+#include "ship.h"
+
+typedef struct Element {
+    Ship *ship;
+    struct Element* next;
+} Element;
+
+typedef struct Fifo {
+    Element * first;
+    Element * last;
+    int size;
+} Fifo;
+
+Fifo create_fifo();
+Element *create_element();
+void line_up(Fifo * _fifo, Ship *_ship);
+Ship *un_queue(Fifo *_fifo);
+void print_fifo(Fifo *_fifo);
+
+#endif //PORTSIMULATOR_FIFO_H
---
+++ src/fifo.c
@@ -0,0 +1,56 @@
+#include "../include/fifo.h"
+
+Fifo create_fifo() {
+    Fifo *fifo = (Fifo*) allocate_memory(sizeof(Fifo));
+    fifo->size=0;
+    fifo->last=NULL;
+    fifo->first=NULL;
+
+    return *fifo;
+}
+
+Element *create_element() {
+    Element *element = (Element *) allocate_memory(sizeof(Element));
+    element->ship = NULL;
+    element->next = NULL;
+
+    return element;
+}
+
+
+void line_up(Fifo * _fifo, Ship *_ship) {
+    Element *element = create_element();
+    element->ship = _ship;
+
+    if (_fifo->first == NULL) {
+        _fifo->first = element;
+        _fifo->last = element;
+    } else {
+        _fifo->last->next = element;
+        _fifo->last = element;
+    }
+
+    _fifo->size++;
+}
+
+Ship *un_queue(Fifo *_fifo){
+    Element *element=_fifo->first;
+    Ship *ship=element->ship;
+
+    _fifo->first=_fifo->first->next;
+    free_memory(ship);
+    if(_fifo->first == NULL)_fifo->last=NULL;
+
+    _fifo->size--;
+    return ship;
+}
+
+void print_fifo(Fifo *_fifo){
+    Element *element = _fifo->first;
+
+    while (element!=NULL){
+        printf("\nNavio %d", element->ship->id);
+        element=element->next;
+    }
+}
+
