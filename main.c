b23ac7bd9fa6a1521fdf4287af9755bf179f8b74
Joao-Lamounier <jjoaolamounier@gmail.com>
2023-05-31 19:19:56 -0300

    Implementado o balanceamento das filas de navios

--- main.c
+++ main.c
@@ -1,25 +1,26 @@
 #include <stdio.h>
-#include "include/container.h"
-#include "include/ship.h"
-#include "include/utils.h"
-#include "include/fifo.h"
+#include "items/include/container.h"
+#include "items/include/ship.h"
+#include "items/include/utils.h"
+#include "items/include/fifo.h"
+#include "services/docking_area.h"
 
 
 
 int main() {
     srand(time(NULL));
 
-    Fifo fifo = create_fifo();
-
+    Fifo fifo[4];
     for (int i = 0; i < 4; ++i) {
-        Ship *ship = ship_arrived();
-        line_up(&fifo, ship);
+        fifo[i]=create_fifo();
     }
+    docking_manager(fifo);
 
-
+    printf("\noi");
     for (int i = 0; i < 4; ++i) {
         printf("\nFila %d", i+1);
-        print_fifo(&fifo);
+        print_fifo(&fifo[i]);
     }
+
     return 0;
 }
---
+++ services/docking_area.h
@@ -0,0 +1,8 @@
+#ifndef PORTSIMULATOR_DOCKING_AREA_H
+#define PORTSIMULATOR_DOCKING_AREA_H
+
+#include "../items/include/fifo.h"
+
+void docking_manager(Fifo _fifos[]);
+
+#endif //PORTSIMULATOR_DOCKING_AREA_H
